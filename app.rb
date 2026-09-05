6beb8c6ea99d3404abf5e0da679476c083bfd2e9
Sasha Lohozinskyy <0x0sky@proton.me>
2026-09-05 09:15:47 +0300

    fix: normalize Ruby bootstrap style

--- app.rb
+++ app.rb
@@ -1,27 +1,28 @@
+# frozen_string_literal: true
+
 # © 2026 aiaiaiai · aiaiaiai.org
 # Repository license is not selected yet; no SPDX identifier is asserted here.
 
-# frozen_string_literal: true
-
-require "json"
-require "roda"
+require 'json'
+require 'roda'
 
 module Aiaiaiai
   module Errors
+    # HTTP entry point for the 4x-errors collector.
     class App < Roda
       plugin :json
 
       route do |r|
         r.root do
           {
-            service: "4x-errors",
-            status: "ok"
+            service: '4x-errors',
+            status: 'ok'
           }
         end
 
-        r.get "health" do
+        r.get 'health' do
           {
-            status: "ok"
+            status: 'ok'
           }
         end
       end
