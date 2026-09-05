4e73a865d1f98373774bb1f32548762691d18ce7
Sasha Lohozinskyy <0x0sky@proton.me>
2026-09-05 17:44:21 +0300

    chore: align browser SPDX header

--- sdk/browser/src/index.js
+++ sdk/browser/src/index.js
@@ -1,5 +1,5 @@
 // © 2026 aiaiaiai · aiaiaiai.org
-// Repository license is not selected yet; no SPDX identifier is asserted here.
+// SPDX-License-Identifier: Apache-2.0
 
 import { createEventDeduplicator } from './deduplicator.js';
 import { attachGlobalErrorCapture } from './global_capture.js';
