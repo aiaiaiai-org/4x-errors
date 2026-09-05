d5d26bbe42ec62b9345441f918e4e4b44d5a5737
Sasha Lohozinskyy <0x0sky@proton.me>
2026-09-05 09:15:59 +0300

    fix: align specs with Ruby namespace

---
+++ spec/aiaiaiai/errors/app_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# © 2026 aiaiaiai · aiaiaiai.org
+# Repository license is not selected yet; no SPDX identifier is asserted here.
+
+require 'rack/test'
+require 'rspec'
+require_relative '../../../app'
+
+RSpec.describe Aiaiaiai::Errors::App do
+  include Rack::Test::Methods
+
+  def app
+    described_class.app
+  end
+
+  it 'exposes a health endpoint' do
+    get '/health'
+
+    expect(last_response.status).to eq(200)
+    expect(JSON.parse(last_response.body)).to eq('status' => 'ok')
+  end
+
+  it 'identifies the service at the root' do
+    get '/'
+
+    expect(last_response.status).to eq(200)
+    expect(JSON.parse(last_response.body)).to include('service' => '4x-errors', 'status' => 'ok')
+  end
+end
