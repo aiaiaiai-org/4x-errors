86c5280b777d718fe0262e57b7cf34d88fb8fbbe
Sasha Lohozinskyy <0x0sky@proton.me>
2026-09-05 14:02:44 +0300

    refactor: keep collector entry point compact

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@
 
 require 'json'
 require 'roda'
+require_relative 'lib/aiaiaiai/errors/browser_ingest'
 require_relative 'lib/aiaiaiai/errors/browser_ingest_policy'
 require_relative 'lib/aiaiaiai/errors/event_store'
 require_relative 'lib/aiaiaiai/errors/event_validator'
@@ -15,6 +16,8 @@ module Aiaiaiai
   module Errors
     # HTTP entry point for the 4x-errors collector.
     class App < Roda
+      include BrowserIngest
+
       MAX_REQUEST_BYTES = 524_288
 
       class << self
@@ -50,25 +53,6 @@ module Aiaiaiai
         persist_event(event)
       end
 
-      def ingest_browser_event(request)
-        ensure_browser_configured!(request)
-        origin = require_browser_origin!(request)
-        apply_browser_cors(origin)
-        event = parse_json(request, read_event_body(request))
-        validate_event(request, event)
-        authorize_browser_project(request, event, origin)
-        persist_event(event)
-      end
-
-      def browser_preflight(request)
-        ensure_browser_configured!(request)
-        origin = require_browser_origin!(request)
-        apply_browser_cors(origin)
-        apply_preflight_headers
-        response.status = 204
-        ''
-      end
-
       def read_event_body(request)
         body = request.body.read(MAX_REQUEST_BYTES + 1)
         request.halt json_error(413, 'request_too_large') if body.bytesize > MAX_REQUEST_BYTES
@@ -79,19 +63,6 @@ module Aiaiaiai
         JSON.parse(body)
       rescue JSON::ParserError
         request.halt json_error(400, 'invalid_json')
-      end
-
-      def authorize_event(request, event)
-        authorization = request.env['HTTP_AUTHORIZATION']
-        project = event.is_a?(Hash) ? event['project'] : nil
-        authenticated = self.class.authenticator.authenticated?(authorization, project)
-        request.halt json_error(401, 'unauthorized') unless authenticated
-      end
-
-      def authorize_browser_project(request, event, origin)
-        project = event.is_a?(Hash) ? event['project'] : nil
-        allowed = self.class.browser_policy.project_origin_allowed?(project, origin)
-        request.halt json_error(403, 'project_origin_not_allowed') unless allowed
       end
 
       def validate_event(request, event)
@@ -105,46 +76,24 @@ module Aiaiaiai
         { event_id: event_id }
       end
 
-      def trusted_collector_configured?
-        self.class.event_store && self.class.authenticator
-      end
-
-      def browser_collector_configured?
-        self.class.event_store && self.class.browser_policy&.configured?
-      end
-
-      def ensure_browser_configured!(request)
-        return if browser_collector_configured?
-
-        request.halt json_error(503, 'collector_unconfigured')
-      end
-
-      def require_browser_origin!(request)
-        origin = request.env['HTTP_ORIGIN']
-        request.halt json_error(403, 'origin_not_allowed') unless browser_origin_allowed?(origin)
-        origin
-      end
-
-      def browser_origin_allowed?(origin)
-        origin && self.class.browser_policy.origin_allowed?(origin)
-      end
-
-      def apply_browser_cors(origin)
-        response['Access-Control-Allow-Origin'] = origin
-        response['Vary'] = 'Origin'
-      end
-
-      def apply_preflight_headers
-        response['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
-        response['Access-Control-Allow-Headers'] = 'Content-Type'
-        response['Access-Control-Max-Age'] = '600'
-      end
-
       def json_error(status, code, details = nil)
         payload = { error: code }
         payload[:details] = details if details
         headers = { 'content-type' => 'application/json' }.merge(response.headers)
         [status, headers, [JSON.generate(payload)]]
+      end
+
+      private
+
+      def authorize_event(request, event)
+        authorization = request.env['HTTP_AUTHORIZATION']
+        project = event.is_a?(Hash) ? event['project'] : nil
+        authenticated = self.class.authenticator.authenticated?(authorization, project)
+        request.halt json_error(401, 'unauthorized') unless authenticated
+      end
+
+      def trusted_collector_configured?
+        self.class.event_store && self.class.authenticator
       end
     end
 
